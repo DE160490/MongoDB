def81b799e3b382204756dbb8c2429b22e299d63
Nguyễn Công Thành <nguyencongthanh15082001@gmail.com>
2024-03-04 14:08:45 +0700

    Cors

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('./cors');
 
 const Dishes = require('../models/dishes');
 const dishRouter = express.Router();
@@ -11,7 +12,8 @@ const user = require('../models/user');
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-    .get((req,res,next) => {
+    .options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
+    .get(cors.cors, (req,res,next) => {
         Dishes.find({})
         .populate('comments.author')
         .then((dishes) => {
@@ -23,7 +25,8 @@ dishRouter.route('/')
     })
 
 dishRouter.route('/:dishId')
-    .get((req,res,next) => {
+    .options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
+    .get(cors.cors, (req,res,next) => {
         Dishes.findById(req.params.dishId)
         .populate('comments.author')
         .then((dish) => {
@@ -36,7 +39,8 @@ dishRouter.route('/:dishId')
 
 
 dishRouter.route('/:dishId/comments')
-    .get((req,res,next) => {
+    .options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
+    .get(cors.cors, (req,res,next) => {
         Dishes.findById(req.params.dishId)
         .populate('comments.author')
         .then((dish) => {
@@ -53,7 +57,7 @@ dishRouter.route('/:dishId/comments')
         }, (err) => next(err))
         .catch((err) => next(err));
     })
-    .post(authenticate.verifyUser, (req, res, next) => {
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         Dishes.findById(req.params.dishId)
         .then((dish) => {
             if (dish != null) {
@@ -80,7 +84,8 @@ dishRouter.route('/:dishId/comments')
     })
 
 dishRouter.route('/:dishId/comments/:commentId')
-    .get((req,res,next) => {
+    .options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
+    .get(cors.cors, (req,res,next) => {
         Dishes.findById(req.params.dishId)
         .populate('comments.author')    
         .then((dish) => {
@@ -102,7 +107,7 @@ dishRouter.route('/:dishId/comments/:commentId')
         }, (err) => next(err))
         .catch((err) => next(err));
     })
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         Dishes.findById(req.params.dishId)
         .then((dish) => {
             if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -136,7 +141,7 @@ dishRouter.route('/:dishId/comments/:commentId')
         }, (err) => next(err))
         .catch((err) => next(err));
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         Dishes.findById(req.params.dishId)
         .then((dish) => {
             if (dish != null && dish.comments.id(req.params.commentId) != null) {
