23f4e579eb0b08db9eed7ac376dc2d1fd1504528
Nguyễn Công Thành <nguyencongthanh15082001@gmail.com>
2024-03-04 11:34:20 +0700

    HTTPS And Upload File

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
@@ -21,3 +22,4 @@ var User = new Schema({
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
+
